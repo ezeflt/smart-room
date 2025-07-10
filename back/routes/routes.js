b572669afaab9ac0a87695be34cf4806cb13dd33
bertino2 <berolbertindjomo@gmail.com>
2025-07-10 17:12:13 +0200

    deletion of the db folder

--- back/routes/routes.js
+++ back/routes/routes.js
@@ -6,4 +6,5 @@ const { register ,getUser,login } = require("../controllers/userController.js");
 router.post("/register", register);
 router.post("/login", login);
 router.get("/user", getUser);
+
 module.exports = router;
