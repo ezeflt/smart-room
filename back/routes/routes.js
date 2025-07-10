1416aa120177e977804b96cff4f51b28ff6c0730
ezeflt <feltenezechiel1@gmail.com>
2025-07-10 17:47:56 +0200

    bug fix user sensor

--- back/routes/routes.js
+++ back/routes/routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { register, getUser } = require("../controllers/userController.js");
+const { register ,getUser,login } = require("../controllers/userController.js");
 
 
 router.post("/register", register);
-
+router.post("/login", login);
+router.get("/user", getUser);
 module.exports = router;
